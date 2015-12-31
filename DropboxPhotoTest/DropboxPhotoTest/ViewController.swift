14d5af2ef044361ee1bc51bbf89ab42da6bc31e4
DuPiMan <bryton@themoellers.net>
2015-12-30 17:33:17 -0800

    adding photo code

---
+++ DropboxPhotoTest/DropboxPhotoTest/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  DropboxPhotoTest
+//
+//  Created by Bryton Moeller on 12/30/15.
+//  Copyright © 2015 citruscircuits. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func shareButtonPressed(sender: AnyObject) {
+        let textToShare = "Swift is awesome!  Check out this website about it!"
+        
+        if let myWebsite = NSURL(string: "http://www.codingexplorer.com/")
+        {
+            let objectsToShare = [textToShare, myWebsite]
+            let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+            
+            self.presentViewController(activityVC, animated: true, completion: nil)
+        }
+    }
+    
+    
+
+    @IBAction func cameraButtonPressed(sender: AnyObject) {
+        
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .Camera
+        picker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(.Camera)!
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+        
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        let activityViewController = UIActivityViewController(activityItems: [info[UIImagePickerControllerOriginalImage] as! UIImage], applicationActivities: nil)
+        presentViewController(activityViewController, animated: true, completion: {})
+    }
+
+}
+
